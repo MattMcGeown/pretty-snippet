a42e44f55fe425714f460fbd42a069ba68026b6c
MattMcGeown <matt_live@live.co.uk>
2024-01-15 21:43:12 +0000

    refactoring to use ui lib components

--- tools/templates/component/__name__.tsx
+++ tools/templates/component/__name__.tsx
@@ -1,12 +1,13 @@
 import type { FC } from 'react';
+import { Box, Heading } from '@chakra-ui/react';
 
 import type { I__name__Props } from './types';
 
 const __name__: FC<I__name__Props> = ({}) => {
   return (
-    <div>
-      <h1>__name__(sentenceCase)</h1>
-    </div>
+    <Box>
+      <Heading as='h1'>__name__(sentenceCase)</Heading>
+    </Box>
   );
 };
 
