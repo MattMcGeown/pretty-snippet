413591513757dc4444f75d25ac5731a68184a5eb
MattMcGeown <matt_live@live.co.uk>
2024-01-20 14:35:16 +0000

    adding editor component to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import { Box, Heading } from '@chakra-ui/react';
+import { Box, Container, Heading } from '@chakra-ui/react';
+
+import { Editor } from '@/components/Editor';
 
 function App() {
   return (
     <Box as="main">
-      <Heading as="h1">Pretty Snippet</Heading>
+      <Container h="100dvh" maxW={{ base: '80vw', md: '60vw', xl: '40vw' }}>
+        <Heading as="h1" textAlign="center">
+          Pretty Snippet
+        </Heading>
+        <Editor />
+      </Container>
     </Box>
   );
 }
