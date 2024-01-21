6907aa7340b0ae6810687986c6bba0a0cc87108d
MattMcGeown <matt_live@live.co.uk>
2024-01-21 20:42:19 +0000

    adding export proof of concept

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,41 @@
 import { Box, Container, Heading } from '@chakra-ui/react';
 
+import { useRef } from 'react';
+import { toPng } from 'html-to-image';
 import { Editor } from '@/components/feature/Editor';
 import { EditorControls } from '@/components/feature/EditorControls';
 
 function App() {
+  const editorWrapperRef = useRef<HTMLDivElement>(null);
+
+  const handleExport = () => {
+    if (editorWrapperRef.current === null) {
+      return;
+    }
+
+    toPng(editorWrapperRef.current, { cacheBust: true })
+      .then((dataUrl) => {
+        // Create a temporary anchor element
+        const downloadLink = document.createElement('a');
+        downloadLink.href = dataUrl;
+
+        // Set the filename for the download
+        downloadLink.download = 'image.png';
+
+        // Append the anchor to the body (required in some browsers)
+        document.body.appendChild(downloadLink);
+
+        // Simulate a click on the anchor to trigger the download
+        downloadLink.click();
+
+        // Remove the anchor from the body
+        document.body.removeChild(downloadLink);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <Box as="main" h="100dvh">
       <Container
@@ -17,8 +49,10 @@ function App() {
         <Heading as="h1" textAlign="center">
           Pretty Snippet
         </Heading>
-        <Editor />
-        <EditorControls />
+        <Box ref={editorWrapperRef}>
+          <Editor />
+        </Box>
+        <EditorControls onExportCallback={handleExport} />
       </Container>
     </Box>
   );
